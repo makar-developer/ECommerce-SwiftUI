08c6a70d1799588c2a052c67509b6a2f9be55490
Makar <makar.coder@gmail.com>
2024-11-16 18:51:57 +0300

    Initial Commit

---
+++ ECommerce/ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceApp.swift
+//  ECommerce
+//
+//  Created by Admin on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
